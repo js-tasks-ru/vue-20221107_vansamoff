deb4030adea9e4cf572ab2b2b604c28333d0058c
Ivan Samoilov <isamoilov@i-core.ru>
2022-12-26 20:20:45 +0300

    solve counterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,16 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    add() {
+      this.counter += 1;
+    },
+  },
+}).mount('#app');
