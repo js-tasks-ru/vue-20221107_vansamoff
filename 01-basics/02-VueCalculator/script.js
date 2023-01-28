a4646b5cdc5240722a1327fa23c4c21f30eb20a2
Ivan Samoylov <vanek.samoylov@gmail.com>
2023-01-28 04:46:39 +0300

    решены задачи 01.02, 01.03

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      first: 0,
+      second: 0,
+      operation: '',
+    };
+  },
+
+  computed: {
+    result() {
+      if (this.operation === 'sum') {
+        return this.first + this.second;
+      }
+      if (this.operation === 'subtract') {
+        return this.first - this.second;
+      }
+      if (this.operation === 'multiply') {
+        return this.first * this.second;
+      }
+      if (this.operation === 'divide') {
+        return this.first / this.second;
+      }
+    },
+  },
+});
+
+const vm = app.mount('#app');
